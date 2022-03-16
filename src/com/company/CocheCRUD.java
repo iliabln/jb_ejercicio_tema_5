117feb3e3a51887200c734241401ca2ebf73e722
iliabln <iliablohin@gmail.com>
2022-03-16 16:49:09 +0100

    Initial commit

---
+++ src/com/company/CocheCRUD.java
@@ -0,0 +1,11 @@
+package com.company;
+
+// save() findAll() delete() que simplemente impriman por consola el nombre del propio método.
+public interface CocheCRUD {
+
+    void save();
+
+    void findAll();
+
+    void delete();
+}
